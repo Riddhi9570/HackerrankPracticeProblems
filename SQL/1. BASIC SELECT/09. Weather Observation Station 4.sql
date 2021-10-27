5ea5863872d0a0f498b70c617284b52395952e3b
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-28 00:16:35 +0530

    Create 09. Weather Observation Station 4.sql

---
+++ SQL/1. BASIC SELECT/09. Weather Observation Station 4.sql
@@ -0,0 +1,6 @@
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
