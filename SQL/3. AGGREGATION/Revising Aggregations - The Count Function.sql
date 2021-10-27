6ee801253b4d5a5d0e150a1405cb439783c22376
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-27 20:46:46 +0530

    The Count Function

---
+++ SQL/3. AGGREGATION/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,7 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+SELECT COUNT(*)
+FROM City
+WHERE Population > 100000;
