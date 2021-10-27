6f0a8a1ca4ddb66962c67fa6e69b1426ddd6dc7e
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-28 02:54:57 +0530

    Create 16. Weather Observation Station 11.sql

---
+++ SQL/1. BASIC SELECT/16. Weather Observation Station 11.sql
@@ -0,0 +1,19 @@
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+-- # Score: 15
+-- # Difficulty: Easy
+
+
+SELECT DISTINCT City FROM Station
+WHERE REGEXP_LIKE(City, '^[^AEIOU]|[^aeiou]$');
+
+
+/* NOTE:
+
+    -DISTINCT :- excludes duplicates.
+    -REGEXP_LIKE() :- This function represents whether the string matches regular expression or not.
+    -[abc] :- It is used to match any characters enclosed in the square brackets.
+    -[^abc] :-	It is used to match any characters not specified in the square brackets.
+    -$ :- The dollar ($) character is used to start matches at the end of a searched string.
+    -p1|p2 :- matches any of the patterns p1 or p2.
+
+*/
