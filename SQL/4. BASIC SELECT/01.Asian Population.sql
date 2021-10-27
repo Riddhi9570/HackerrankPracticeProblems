b3c5cbb8ecb48e12344710fd9a1fa9b19120a5be
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-27 21:01:21 +0530

    Revert "Asian Population"
    This reverts commit 4ef2a9576ea2d6bed98aa607c1e2762a354c5941.

--- SQL/4. BASIC SELECT/01.Asian Population.sql
+++
@@ -1,8 +0,0 @@
--- # Problem: https://www.hackerrank.com/challenges/asian-population/problem
--- # Score: 10
--- # Difficulty: Easy
-
-SELECT SUM(CITY.POPULATION)
-FROM CITY
-JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE
-WHERE COUNTRY.CONTINENT = 'Asia';
