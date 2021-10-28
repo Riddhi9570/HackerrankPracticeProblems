db5ffba6daf894471e61b8a311f3d5e756d0ee2e
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-28 13:45:43 +0530

    Create 19. Employee Names.sql

---
+++ SQL/1. BASIC SELECT/19. Employee Names.sql
@@ -0,0 +1,11 @@
+-- # Problem: https://www.hackerrank.com/challenges/name-of-employees/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+
+SELECT Name
+FROM Employee
+ORDER BY Name;
+
+
+-- # NOTE: 'ORDER BY Name' and 'ORDER BY Name ASC' both sort the result in alphabetical order of Name.
