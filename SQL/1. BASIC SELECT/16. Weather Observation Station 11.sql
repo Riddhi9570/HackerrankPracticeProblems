57be82b8340fccf486438c726f12fe3f4a0a2827
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-28 14:04:11 +0530

    Update 16. Weather Observation Station 11.sql

--- SQL/1. BASIC SELECT/16. Weather Observation Station 11.sql
+++ SQL/1. BASIC SELECT/16. Weather Observation Station 11.sql
@@ -11,7 +11,7 @@ WHERE REGEXP_LIKE(City, '^[^AEIOU]|[^aeiou]$');
 
     -DISTINCT :- excludes duplicates.
     -REGEXP_LIKE() :- This function represents whether the string matches regular expression or not.
-    -[abc] :- It is used to match any characters enclosed in the square brackets.
+    -^ :- The caret (^) character is used to start matches at the beginning of a searched string.
     -[^abc] :-	It is used to match any characters not specified in the square brackets.
     -$ :- The dollar ($) character is used to start matches at the end of a searched string.
     -p1|p2 :- matches any of the patterns p1 or p2.
