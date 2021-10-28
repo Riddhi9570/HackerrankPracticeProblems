8445fb652a12f513087c63df9ad06b774d8656e7
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-28 14:01:32 +0530

    Update 13. Weather Observation Station 8.sql

--- SQL/1. BASIC SELECT/13. Weather Observation Station 8.sql
+++ SQL/1. BASIC SELECT/13. Weather Observation Station 8.sql
@@ -11,9 +11,10 @@ WHERE  city REGEXP '^[AEIOU].*[aeiou]$'
     -DISTINCT excludes duplicates.
     -REGEXP is the operator used when performing regular expression pattern matches.
     -^ :- The caret (^) character is used to start matches at the beginning of a searched string.
-    -$ :- The dollar ($) character is used to start matches at the end of a searched string.
     -[abc] :- It is used to match any characters enclosed in the square brackets.
+    -. :- The dot (.) character matches any single character.
     -* :-The asterisk (*) character matches zero (0) or more instances of the preceding strings.
+    -$ :- The dollar ($) character is used to start matches at the end of a searched string.
     -WHERE city REGEXP '^[AEIOU].*[aeiou]$' is equivalent to WHERE REGEXP_LIKE(City, '^[AEIOU].*[aeiou]$')
     
 */
