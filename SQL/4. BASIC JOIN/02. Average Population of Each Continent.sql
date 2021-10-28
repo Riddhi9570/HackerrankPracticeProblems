ad50854963e7c5218e55426beb016a1d73627fe1
Riddhi Bhagat <riddhi0bhagat@gmail.com>
2021-10-28 21:34:40 +0530

    Update 02. Average Population of Each Continent.sql

--- SQL/4. BASIC JOIN/02. Average Population of Each Continent.sql
+++ SQL/4. BASIC JOIN/02. Average Population of Each Continent.sql
@@ -3,4 +3,23 @@
 -- # Difficulty: Easy
 
 
-SELECT o.CONTINENT, FLOOR(AVG(i.POPULATION)) FROM CITY AS i JOIN COUNTRY AS o ON i.COUNTRYCODE=o.CODE GROUP BY o.CONTINENT;
+SELECT 
+  o.CONTINENT,
+  FLOOR(AVG(i.POPULATION))
+FROM 
+  CITY AS i JOIN COUNTRY AS o 
+  ON i.COUNTRYCODE=o.CODE 
+GROUP BY 
+  o.CONTINENT;
+
+
+/* NOTE:
+
+  - FLOOR() function is used to return the largest integer value which will be either equal to or less than from a given input number.
+  - The AVG() function returns the average value of all records in a field.
+  - AVG() when used with GROUP BY clause, returns the average value of group of records in a field.
+  - JOINS are used to retrieve data from multiple tables.
+  - ON clause serves for conditions that specify how to join tables.
+  - AS is used to give a temporary name to a table or a column in a table for the purpose of a particular query.
+  
+*/
