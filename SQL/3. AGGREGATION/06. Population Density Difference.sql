0e9eae3f0558ec5cbbcc20ed2d40d92d3dab47d7
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-30 23:28:14 +0530

    Create 06. Population Density Difference.sql

---
+++ SQL/3. AGGREGATION/06. Population Density Difference.sql
@@ -0,0 +1,7 @@
+-- # Problem: https://www.hackerrank.com/challenges/population-density-difference/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+
+SELECT MAX(Population) - MIN(Population)
+FROM City;
