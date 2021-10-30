cfe0b54f9e232413f71da835f8b1c6d4c1b92590
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-30 23:09:24 +0530

    Create 03. Revising Aggregations - Averages.sql

---
+++ SQL/3. AGGREGATION/03. Revising Aggregations - Averages.sql
@@ -0,0 +1,8 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+
+SELECT AVG(Population)
+FROM City
+WHERE District = 'California';
