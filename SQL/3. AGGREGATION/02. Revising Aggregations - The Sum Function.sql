97f0358ca36d0205b4af808bc8a88e21cd001795
Riddhi9570 <riddhi0bhagat@gmail.com>
2021-10-30 23:00:43 +0530

    Create 02. Revising Aggregations - The Sum Function.sql

---
+++ SQL/3. AGGREGATION/02. Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,8 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+-- # Score: 10
+-- # Difficulty: Easy
+
+
+SELECT SUM(Population)
+FROM City
+WHERE District = 'California';
