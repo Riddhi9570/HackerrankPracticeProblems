4db8824cef4d42130b891126f855f44ca25c970c
Riddhi Bhagat <riddhi0bhagat@gmail.com>
2021-11-12 00:30:05 +0530

    Created 02. Draw The Triangle 2.sql

---
+++ SQL/6. ALTERNATIVE QUERIES/02. Draw The Triangle 2.sql
@@ -0,0 +1,9 @@
+-- # Problem: https://www.hackerrank.com/challenges/draw-the-triangle-1/problem
+-- # Score: 25
+-- # Difficulty: Easy
+
+
+SET @num = 0;
+SELECT REPEAT('* ', @num := @num + 1) 
+FROM information_schema.tables 
+limit 20;
