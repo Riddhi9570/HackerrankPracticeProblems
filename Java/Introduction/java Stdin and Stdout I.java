8def84412383df6428eb973c9631bb3127363f96
Tamali07 <98543049+Rittika1234@users.noreply.github.com>
2022-10-01 23:59:42 +0530

    Update java Stdin and Stdout I.java

--- Java/Introduction/java Stdin and Stdout I.java
+++ Java/Introduction/java Stdin and Stdout I.java
@@ -11,13 +11,11 @@ public class Solution {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        // Complete this line
-        // Complete this line
+        
     }
 }
