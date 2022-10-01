d2911d734bfc3bbba8c733328cc95f1c9ed102f5
Dhruv <65610188+dhruvbhatia1@users.noreply.github.com>
2022-10-01 10:55:49 +0530

    Stdin Stdout II program Java

--- Java/Introduction/StdIn_StdOut.java
+++ Java/Introduction/StdIn_StdOut.java
@@ -8,7 +8,6 @@ public class Solution {
         double d = scan.nextDouble();
         scan.nextLine();
         String s = scan.nextLine();
-        // Write your code here.
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
