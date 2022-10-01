9eb1e8c55b72246e0289d6d4af496869cea7be42
Himangshu Kalita <himangshuk13@gmail.com>
2022-10-01 15:49:28 +0530

    Added java substring

---
+++ Java/Strings/substring.java
@@ -0,0 +1,14 @@
+package com.javaaid.hackerrank.solutions.languages.java.strings;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		System.out.println(S.substring(start, end));
+		in.close();
+	}
+}
