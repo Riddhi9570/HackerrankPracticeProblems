fd8a4f9f1f46e8699e0d9792235ad90f759b958b
Prahmodh R <89332177+Prahmodh-R@users.noreply.github.com>
2022-10-01 12:54:29 +0530

    Created Variable sized arrays-cpp

---
+++ C++/Introduction/Variable_Sized_Arrays.cpp
@@ -0,0 +1,30 @@
+//Hackerrank Problem link: https://www.hackerrank.com/challenges/variable-sized-arrays/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() { 
+    int n,q;
+    cin>>n>>q;
+    vector<vector<int>> varlength;
+    for(int i=0;i<n;i++){
+        int len;
+        cin>>len;
+        vector<int> curind(len);
+        for(int j=0;j<len;j++){
+            cin>>curind[j];
+        }
+        varlength.push_back(curind);
+    }
+    while(q--){
+        int outerind,innerind;
+        cin>>outerind>>innerind;
+        cout<<varlength[outerind][innerind]<<"\n";
+    }
+    return 0;
+}
