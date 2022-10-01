17ee371f170f29d9e2bfc1f256901cc36c6878cb
saikishore222 <saikishore.chsk@gmail.com>
2022-10-01 14:41:34 +0530

    Conversion
    Java Program to Convert int to String

---
+++ Java/JavaIntToString.java
@@ -0,0 +1,52 @@
+
+//-- # Problem : https://www.hackerrank.com/challenges/java-int-to-string/problem?isFullScreen=true
+
+//-- # Score : 10
+
+//-- # Difficulty : Easy
+
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //this line Converts Int to String
+    String s=Integer.toString(n);   
+    //it checks whether it was converted or not
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
