f4209d934eab3033f41af9747abbff6b1d773cb4
Tamali07 <98543049+Rittika1234@users.noreply.github.com>
2022-10-01 16:59:01 +0530

    Java Substring

---
+++ Java/Strings/Java Substring.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.next();
+        int start=sc.nextInt();
+        int end= sc.nextInt();
+        System.out.println(s.substring(start,end));
+    }
+}
