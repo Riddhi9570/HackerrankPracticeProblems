eb6625b3d07d2356445e2c732d84aefdeb5a978d
Anjali2201 <anjalikushwaha031@gmail.com>
2022-10-01 21:52:20 +0530

    Python: Arithemetic Operators

---
+++ Python/Introduction/Arithemetic Operators.py
@@ -0,0 +1,12 @@
+# Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Score: 10
+# dificulty: Easy
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
