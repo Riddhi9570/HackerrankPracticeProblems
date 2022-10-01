06e67511361a4c0a2c9e15dc75ef6a3f439c19dc
Sumit Bisht <sumit7beazt@gmail.com>
2022-10-01 13:31:15 +0530

    minor changes

--- C++/Introduction/Say_Hello_World_With_C++.cpp
+++ C++/Introduction/Say_Hello_World_With_C++.cpp
@@ -1,4 +1,4 @@
-// Problem : https : // www.hackerrank.com/challenges/cpp-hello-world/problem
+// Problem : https://www.hackerrank.com/challenges/cpp-hello-world/problem
 // Score: 5.0
 // Difficulty : Easy
 
