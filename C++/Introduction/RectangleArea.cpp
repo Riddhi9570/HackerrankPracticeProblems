e3d9f698365028caa7f5cbf93097f659b2cacea4
Dhruv-Mahajan <dhruv.mahajan001@gmail.com>
2022-10-01 15:25:07 +0530

    Solves Rectangle area Problem

---
+++ C++/Introduction/RectangleArea.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int h,w;
+    //taking input 
+    cin>>h>>w;
+    //print height and width
+    cout<<h<<" "<<w<<endl;
+    // print the calculated area 
+    cout<<h*w ;
+    return 0;
+}
