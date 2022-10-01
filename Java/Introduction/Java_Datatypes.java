b91e7bf1c5b45eaeaefedca47ba221e04e2aa36d
Anjali2201 <anjalikushwaha031@gmail.com>
2022-10-01 21:40:46 +0530

    Added Java Introduction Practice Problem "Java_Datatypes'

---
+++ Java/Introduction/Java_Datatypes.java
@@ -0,0 +1,36 @@
+// Problem : https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
+// Score : 10
+// Difficulty : Easy
+
+
+
+import java.util.*;
+
+class Solution{
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                if(x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+        sc.close();
+    }
+}
+
+
+
