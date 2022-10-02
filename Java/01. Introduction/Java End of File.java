08edf3b30f722fd9e3294fdd41f57ff724086501
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2022-10-02 08:37:24 +0530

    Added Java EOF

---
+++ Java/01. Introduction/Java End of File.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1; scan.hasNext()== true; i++){
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
