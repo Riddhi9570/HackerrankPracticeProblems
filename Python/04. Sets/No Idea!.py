7e7bf3b866442c7555eda55a4285dd87f95f4a33
Shivaye <shivaye241199modi@gmail.com>
2022-10-02 15:52:45 +0530

    Added solution to No Idea! problem in Python/04. Sets folder

---
+++ Python/04. Sets/No Idea!.py
@@ -0,0 +1,19 @@
+io = input().split()
+m = int(io[0])
+n = int(io[1])
+
+storage = list()
+count = 0
+
+storage = list(map(int, input().strip().split()))
+
+A = set(map(int, input().strip().split()))
+B = set(map(int, input().strip().split()))
+
+for i in storage:
+    if i in A:
+        count = count+1
+    if i in B:
+        count = count-1
+
+print(count)
