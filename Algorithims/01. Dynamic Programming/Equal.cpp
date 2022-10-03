24ab21a28aff3b6a1e7f1b05389a972fba7b5b8d
Sasidhar Swarangi <44099792+sasidharswarangi@users.noreply.github.com>
2022-10-03 09:43:48 +0530

    Update Equal.cpp
    added comments to Equal.cpp

--- Algorithims/01. Dynamic Programming/Equal.cpp
+++ Algorithims/01. Dynamic Programming/Equal.cpp
@@ -14,17 +14,17 @@ int main() {
         assert(N <= 10000);
         for(int i = 0; i < N; i++) 
         {cin >> d[i]; assert(d[i] < 1000);}
-        int M = *min_element(d, d + N);
-        int r = 1e9;
+        int M = *min_element(d, d + N);//finding the minimum element
+        int r = 1e9; //just an upper limit
         for(int t = M - 4; t <= M; t++) {
             int s = 0;
             for(int i = 0; i < N; i++) {
                 int D = d[i] - t;
-                s += D / 5, D %= 5;
-                s += D / 2, D %= 2;
+                s += D / 5, D %= 5;//we are trying to find the number of times the given number be divivisible by 5->gives no of opertations
+                s += D / 2, D %= 2;//same with 2 and 1 from the remainder
                 s += D;
             }
-            r = min(r, s);
+            r = min(r, s); //just an upperlimit for testing
         }
         cout << r << endl;
     }
